64a55bd37227dc15998227fbf9443ed134efb9d9
Xirdion <t.voggenreiter01@gmail.com>
2017-10-30 15:41:18 +0100

    fixed plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -13,14 +13,21 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Dreibein\ArticleBackgroundBundle\DreibeinArticleBackgroundBundle;
 
+/**
+ * Class Plugin
+ * @package ContaoManager
+ */
 class Plugin implements BundlePluginInterface
 {
+    /**
+     * @param ParserInterface $parser
+     * @return array
+     */
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(DreibeinArticleBackgroundBundle::class)->setLoadAfter([ContaoCoreBundle::class])
+            BundleConfig::create('Dreibein\ArticleBackgroundBundle\DreibeinArticleBackgroundBundle')->setLoadAfter([ContaoCoreBundle::class])
         ];
     }
 
